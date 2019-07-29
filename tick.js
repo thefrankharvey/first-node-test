743c61d1a6e56ee03c563ad22155e097e5c7bebd
Frank <frank@thefrankharvey.com>
2019-07-29 16:15:41 -0400

    added comments

--- tick.js
+++ tick.js
@@ -1,5 +1,12 @@
 console.log('start')
 process.nextTick(function() {
     console.log('nexTick callback');
-})
-console.log('scheduled')
+});
+//'scheduled' runs second not third
+//node uses event loop processing
+//looks for next executable event
+//the callback is inside a function
+//so it executes on the next loop
+//similar to timeout but doesn't require
+//any i/o from the client
+console.log('scheduled');
