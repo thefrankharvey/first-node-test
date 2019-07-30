956997ecbf30abee862c75f183ba93d655b50ec0
Frank <frank@thefrankharvey.com>
2019-07-30 12:13:13 -0400

    clean up file tree

--- egghead-intro-to-node/intro2/read-file.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const fs = require('fs');
-
-const fileName = './node-test.txt';
-fs.readFile(fileName, function (err, data) {
-   console.log(data);
-   console.log(Buffer.byteLength(data))
-});
